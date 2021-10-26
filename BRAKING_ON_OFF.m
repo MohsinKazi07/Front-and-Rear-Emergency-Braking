da451c920f28db5cb245db5f19af40de5e902c60
sonnep <91832018+sonnep@users.noreply.github.com>
2021-10-26 19:06:20 +0530

    Add files via upload
    Project_file

---
+++ BRAKING_ON_OFF.m
@@ -0,0 +1,13 @@
+classdef BRAKING_ON_OFF < Simulink.IntEnumType
+    enumeration
+        NONE(0)
+        FRONT_WARNING_ON(1)
+        WARNING_OFF(2)
+        FRONT_BRAKING_ON(3)
+        BRAKING_OFF(4)
+        REAR_WARNING_ON(5)
+        REAR_BRAKING_ON(6)
+    
+    end
+    
+end
---
+++ MUST_REQ.m
@@ -0,0 +1,14 @@
+classdef MUST_REQ < Simulink.IntEnumType
+    enumeration
+        IGNITION_OFF(0)
+        IGNITION_ON(1)
+        EMERGENCY_BRAKING_OFF(0)
+        EMERGENCY_BRAKING_ON(1)
+        DRIVE_MODE_OFF(0)
+        DRIVE_MODE_ON(1)
+        REAR_MODE_OFF(0)
+        REAR_MODE_ON(1)
+        COMPARE_WITH_ALL(1)
+     end
+    
+end
